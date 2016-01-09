ec52a53c04d723eee7e9129c5479111649f683cd
James Stewart <admin@jamesstewy.com>
2016-01-09 18:31:55 +1100

    rewrite to work with multiple docker servers

--- lib/synapse/service_watcher/docker2.rb
+++ lib/synapse/service_watcher/docker2.rb
@@ -1,31 +1,141 @@
 require "synapse/service_watcher/base"
 require 'docker'
 
+$docker2_servers = {}
+
+class Docker2Server
+  include Synapse::Logging
+  
+  attr_reader :name
+  attr_reader :service_host
+  
+  def initialize(name, url, service_host, event_whitelist = ['start', 'die'])  
+    @name = name
+    @url = url
+    @service_host = service_host
+    @event_whitelist = event_whitelist
+    @queues = []
+    @cnts = []
+    @cnts_lock = Mutex.new
+    @watcher = Thread.new {
+      run
+    }
+  end
+  
+  def cnts
+    @cnts_lock.synchronize {
+      @cnts
+    }
+  end
+  
+  def register
+    queue = Queue.new
+    @queues << queue
+    return queue
+  end
+  
+  def run
+    until @should_exit
+      get_containers
+      send(true)
+      begin
+        Docker.url = @url
+        Docker::Event.stream { |event|
+          Thread.new {
+            if @event_whitelist.include? event.status
+              get_containers
+              send(event)
+            end
+          }
+        }
+      rescue Docker::Error::TimeoutError => e
+        next
+      rescue Exception => e
+        raise e
+      end
+    end
+  end
+
+  def send(msg)
+    @queues.map do |queue|
+      queue << msg
+    end
+  end
+
+  def rewrite_container_ports(ports)
+    pairs = []
+    if ports.is_a?(String)
+      # "Ports" comes through (as of 0.6.5) as a string like "0.0.0.0:49153->6379/tcp, 0.0.0.0:49153->6379/tcp"
+      # Convert string to a map of container port to host port: {"7000"->"49158", "6379": "49159"}
+      pairs = ports.split(", ").collect do |v|
+        pair = v.split('->')
+        [ pair[1].rpartition("/").first, pair[0].rpartition(":").last ]
+      end
+    elsif ports.is_a?(Array)
+      # New style API, ports is an array of hashes, with numeric values (or nil if no ports forwarded)
+      pairs = ports.collect do |v|
+        [v['PrivatePort'].to_s, v['PublicPort'].to_s]
+      end
+    end
+    Hash[pairs]
+  end
+
+  def get_containers
+    @cnts_lock.synchronize {
+      begin
+        Docker.url = @url
+        @cnts = Docker::Util.parse_json(Docker.connection.get('/containers/json', {}))
+        
+        @cnts.each do |cnt|
+          cnt['Ports'] = rewrite_container_ports(cnt['Ports'])
+        end
+      rescue => e
+        log.warn "synapse: error polling docker host #{@url}: #{e.inspect}"
+        @cnts = []
+      end
+    }
+  end
+end
+
 class Synapse::ServiceWatcher
   class Docker2Watcher < BaseWatcher
     def start
-      @queue = Mutex.new
       @wait_interval = @discovery['wait_interval'] || 0 # ms
-      @events = ['start', 'die']
-      @latest_time = -1
-      @watcher = Thread.new do
-        until @should_exit
-          begin
-            set_backends(containers)
-            Docker::Event.stream {
-              |event|
-              Thread.new do
-                event_handle(event)
+      @event_lock = Mutex.new
+      @servers = []
+      
+      @discovery['servers'].map do |server|
+        # determine url
+        if server['protocol'] == 'unix'
+          url = "unix://#{server['host']}"
+          service_host = '127.0.0.1'
+        else
+          url = "#{server['protocol'] || 'http'}://#{server['host']}:#{server['port'] || 4243}"
+          service_host = server['host']
+        end
+        
+        # create new server if it doesn't exists
+        if not $docker2_servers.has_key?(url)
+          $docker2_servers[url] = Docker2Server.new(server['name'], url, service_host)
+          log.info "synapse: #{$docker2_servers.length} docker2 servers running."
+        end
+        
+        @servers << url
+        
+        Thread.new {
+          queue = $docker2_servers[url].register
+          until @should_exit
+            event = queue.pop
+            Thread.new {
+              if @event_lock.try_lock
+                sleep(@wait_interval)
+                set_backends(containers)
+                @event_lock.unlock
               end
             }
-          rescue Docker::Error::TimeoutError => e
-            next
-          rescue Exception => e
-            raise e
           end
-        end
+        }
       end
-      log.info "synapse: docker2 watcher exited successfully"
     end
 
     private
@@ -45,76 +155,9 @@ class Synapse::ServiceWatcher
       end
     end
 
-    def event_handle(event)
-      begin
-        # exit if event is not in @events
-        unless @events.include? event.status
-          return
-        end
-
-        @queue.lock # wait for lock
-
-        # check if event has already been handled
-        if event.time <= @latest_time
-          @queue.unlock
-          return
-        end
-
-        sleep_until_next_check(event.time) # wait for @wait_interval seconds
-
-        set_backends(containers) # call docker
-        @queue.unlock # unlock queue
-      rescue Exception => e
-        log.warn "synapse: error in watcher thread: #{e.inspect}"
-        log.warn e.backtrace
-      end
-    end
-
-    def sleep_until_next_check(start_time)
-      sleep_time = @wait_interval - (Time.now.to_i - start_time)
-      if sleep_time > 0.0
-        sleep(sleep_time)
-      end
-    end
-
-    def rewrite_container_ports(ports)
-      pairs = []
-      if ports.is_a?(String)
-        # "Ports" comes through (as of 0.6.5) as a string like "0.0.0.0:49153->6379/tcp, 0.0.0.0:49153->6379/tcp"
-        # Convert string to a map of container port to host port: {"7000"->"49158", "6379": "49159"}
-        pairs = ports.split(", ").collect do |v|
-          pair = v.split('->')
-          [ pair[1].rpartition("/").first, pair[0].rpartition(":").last ]
-        end
-      elsif ports.is_a?(Array)
-        # New style API, ports is an array of hashes, with numeric values (or nil if no ports forwarded)
-        pairs = ports.collect do |v|
-          [v['PrivatePort'].to_s, v['PublicPort'].to_s]
-        end
-      end
-      Hash[pairs]
-    end
-
     def containers
-      backends = @discovery['servers'].map do |server|
-        server['protocol'] = server['protocol'] || 'http'
-        if server['protocol'] == 'unix'
-          Docker.url = "unix://#{server['host']}"
-          service_host = '127.0.0.1'
-        else
-          Docker.url = "#{server['protocol']}://#{server['host']}:#{server['port'] || 4243}"
-          service_host = server['host']
-        end
-        begin
-          cnts = Docker::Util.parse_json(Docker.connection.get('/containers/json', {}))
-          @latest_time = Time.now.to_i
-        rescue => e
-          log.warn "synapse: error polling docker host #{Docker.url}: #{e.inspect}"
-          next []
-        end
-        cnts.each do |cnt|
-          cnt['Ports'] = rewrite_container_ports cnt['Ports']
-        end
+      backends = @servers.map do |url|
+        cnts = $docker2_servers[url].cnts
         # Discover containers that match the image/port we're interested in
         cnts = cnts.find_all do |cnt|
           cnt["Image"].partition(":").first == @discovery["image_name"] \
@@ -122,8 +165,8 @@ class Synapse::ServiceWatcher
         end
         cnts.map do |cnt|
           {
-            'name' => server['name'],
-            'host' => service_host,
+            'name' => $docker2_servers[url].name,
+            'host' => $docker2_servers[url].service_host,
             'port' => cnt["Ports"][@discovery["container_port"].to_s()]
           }
         end
